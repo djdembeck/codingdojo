b04f3fa89d6fb93fcf5780b8853dcbac3bcf543d
djdembeck <dembeck.david@protonmail.com>
2021-03-29 13:37:32 -0500

    finished assignment

--- 1-webfund/js/practice-loops.js
+++ 1-webfund/js/practice-loops.js
@@ -4,8 +4,8 @@ for (let i = 1; i < 20; i+=2) {
 }
 
 // Sum numbers from 1 to 5, printing out the current number and sum so far at each step of the way
-// Still need to fix this one
-for (var i = 1; i <= 15; i++) {
-    var j = i++
+var j = 0
+for (var i = 1; i <= 5; i++) {
+    var j = (j + i)
     console.log("Num: " + i + ", Sum: " + j)
 }
