2c0d20a4d1754c87053baefb73c6608b30c4518a
djdembeck <dembeck.david@protonmail.com>
2021-03-29 11:42:31 -0500

    solved

--- 1-webfund/js/predict-loops.js
+++ 1-webfund/js/predict-loops.js
@@ -3,12 +3,17 @@ for(var num = 0; num < 15; num++){
     console.log(num);
 }
 
+// 1-14 will be logged
+
 // 2
 for(var i = 1; i < 10; i+=2){
     if(i % 3 == 0){
         console.log(i);
     }
 }
+
+// checks happen at 1, 3, 5, 7, 9
+// 3, 9 logged
 
 // 3
 for(var j = 1; j <= 15; j++){
@@ -20,3 +25,7 @@ for(var j = 1; j <= 15; j++){
     }
     console.log(j);
 }
+
+// 1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 16
+
+// 1, 4, 5, 8, 10, 11, 14, 16 logged
