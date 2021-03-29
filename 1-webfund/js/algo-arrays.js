3d1885310cf22df8590a5372cc8af70f33df3506
djdembeck <dembeck.david@protonmail.com>
2021-03-29 17:10:05 -0500

    start part 2

--- 1-webfund/js/algo-arrays.js
+++ 1-webfund/js/algo-arrays.js
@@ -1,3 +1,4 @@
+// 1
 // Using the given array:
 // varcopy testArr = [6,3,5,1,2,4]
 // Print each array value and the sum so far
@@ -8,3 +9,6 @@ for(var i = 0; i < testArr.length; i++){
     var j = (j + testArr[i])
     console.log("Num: " + testArr[i] + ", Sum: " + j);
 }
+
+// 2
+// Multiply each value in the array by its array position
