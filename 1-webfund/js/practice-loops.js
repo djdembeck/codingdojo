d1731eb4d8f216ef0c79ad7ab23ae6a87ddd380d
djdembeck <dembeck.david@protonmail.com>
2021-03-29 12:06:01 -0500

    submit work so far

--- 1-webfund/js/practice-loops.js
+++ 1-webfund/js/practice-loops.js
@@ -1,4 +1,11 @@
 // Print out all odd numbers from 1 to 20
-
+for (let i = 1; i < 20; i+=2) {
+    console.log(i)
+}
 
 // Sum numbers from 1 to 5, printing out the current number and sum so far at each step of the way
+// Still need to fix this one
+for (var i = 1; i <= 15; i++) {
+    var j = i++
+    console.log("Num: " + i + ", Sum: " + j)
+}
