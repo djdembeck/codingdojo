dc86aa1fed2af1222536b4b243a37548d7c30936
djdembeck <dembeck.david@protonmail.com>
2021-03-29 17:13:28 -0500

    finish assignment, move to day 2

--- 1-webfund/js/day-2/algo-arrays.js
+++ 1-webfund/js/day-2/algo-arrays.js
@@ -12,3 +12,10 @@ for(var i = 0; i < testArr.length; i++){
 
 // 2
 // Multiply each value in the array by its array position
+
+var testArr = [6,3,5,1,2,4]
+for(var i = 0; i < testArr.length; i++){
+    testArr[i] = testArr[i] * i;
+}
+
+console.log(testArr);
