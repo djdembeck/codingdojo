8dfa07d53233d9a22b419f462c203036361beefa
djdembeck <dembeck.david@protonmail.com>
2021-03-29 17:04:29 -0500

    finished assignment

--- 1-webfund/js/day-1/predict-arrays.js
+++ 1-webfund/js/day-1/predict-arrays.js
@@ -3,6 +3,8 @@ var arr = [8,6,7,5,3,0,9]
 for(var i = 0; i < arr.length; i++){    
     console.log(arr[i]);
 }
+// This is going to list everything in the array, one by one, in order
+// 8, 6, 7, 5, 3, 0, 9
 
 // 2
 var arr = [7,3,8,4,2,0,1];
@@ -14,6 +16,7 @@ for(var i = 0; i < arr.length; i++){
         console.log("That is odd!");
     }
 }
+// That is odd!, That is odd!, 8, 4, 2, 0, That is odd!
 
 // 3
 var arr = [1,3,8,-5,0,-2,4,-1];
@@ -32,3 +35,6 @@ for(var i = 0; i< arr.length; i++){
 }
 console.log(arr);
 console.log(newArr);
+
+// var arr = [-1,-3,-8,5,"Zero",2,-4,1];
+// var newArr = [-5, -2, -1];
