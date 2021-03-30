b0c0a22683d9b7690c60489a7f4e0768f74da59d
djdembeck <dembeck.david@protonmail.com>
2021-03-30 14:39:13 -0500

    can re-use numtostring for this

--- 1-webfund/js/day-2/basic-foundation-2.js
+++ 1-webfund/js/day-2/basic-foundation-2.js
@@ -1,5 +1,16 @@
 // Biggie Size - Given an array, write a function that changes all positive numbers in the array to the string "big".  Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1, "big", "big", -5].
+function numtostring(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[i] = "big"
+        }
+    }
+    return arr;
+}
 
+var somearray = [-1,3,5,-5];
+
+console.log(numtostring(somearray));
 
 // Print Low, Return High - Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
 
