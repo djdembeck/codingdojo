2acbcdc404a4b2a48a11eb49c57b56722adf34e0
djdembeck <dembeck.david@protonmail.com>
2021-03-30 13:16:57 -0500

    fix problem 2

--- 1-webfund/js/day-2/basic-foundation-1.js
+++ 1-webfund/js/day-2/basic-foundation-1.js
@@ -11,13 +11,11 @@ console.log(count());
 
 // Get even 1000 - Write a function that would get the sum of all the even numbers from 1 to 1000.  You may use a modulus operator for this exercise.
 function even1000() {
-    even = []
+    var sum = 0
     for (let i = 0; i <= 1000; i++) {
         if (i % 2 === 0) {
-            console.log(i)
-            even.push(i)
+            var sum = sum + i;
         }
-        var sum =+ even[i];
     }
     return sum;
 }
