0e7aa5f27379eca5d871a8b97d53686999b549ed
djdembeck <dembeck.david@protonmail.com>
2021-03-30 14:11:51 -0500

    complete replaceneg

--- 1-webfund/js/day-2/basic-foundation-1.js
+++ 1-webfund/js/day-2/basic-foundation-1.js
@@ -130,6 +130,19 @@ console.log(square(somearray));
 
 // Negatives - Given an array with multiple values, write a function that replaces any negative numbers within the array with the value of 0. When the program is done the array should contain no negative values. (e.g. [1,5,10,-2] will become [1,5,10,0])
 
+function replaceneg(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        if (num < 0) {
+            arr[i] = 0
+        }
+    }
+    return arr;
+}
+
+var somearray = [1,5,10,-2];
+
+console.log(replaceneg(somearray));
 
 // Max/Min/Avg - Given an array with multiple values, write a function that returns a new array that only contains the maximum, minimum, and average values of the original array. (e.g. [1,5,10,-2] will return [10,-2,3.5])
 
