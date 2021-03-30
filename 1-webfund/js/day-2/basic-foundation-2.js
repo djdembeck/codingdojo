a4442b029f55ebc682f49c82d037b397c5b94950
djdembeck <dembeck.david@protonmail.com>
2021-03-30 16:38:01 -0500

    this took longer than I care to admit

--- 1-webfund/js/day-2/basic-foundation-2.js
+++ 1-webfund/js/day-2/basic-foundation-2.js
@@ -145,6 +145,19 @@ var somearray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 console.log(incrseconds(somearray));
 
 // Previous Lengths - You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
+function prevlengths(arr) {
+    var strlength;
+    for (let i = arr.length - 1; i > 0; i--){
+        let prev = i - 1;
+        strlength = arr[prev].length;
+        arr[i] = strlength;
+    }
+    return arr;
+}
+
+var somearray = ["hello", "dojo", "awesome"];
+
+console.log(prevlengths(somearray));
 
 
 // Add Seven - Build a function that accepts an array. Return a new array with all the values of the original, but add 7 to each. Do not alter the original array.  Example, addSeven([1,2,3]) should return [8,9,10] in a new array.
