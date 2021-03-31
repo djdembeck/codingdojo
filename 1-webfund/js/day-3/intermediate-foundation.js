b1b8518d889b388d547e8b74e837658621d62da2
djdembeck <dembeck.david@protonmail.com>
2021-03-31 14:18:47 -0500

    i hate Fibo

--- 1-webfund/js/day-3/intermediate-foundation.js
+++ 1-webfund/js/day-3/intermediate-foundation.js
@@ -24,6 +24,20 @@ console.log(factorial(y));
 
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  Do this without using recursion first.  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment.
 
+function fibo(num) {
+    var startnum = 1;
+    var secondarynum = 0;
+    var holder;
+    for (let i = num - 1; i >= 0; i--) {
+        holder = startnum;
+        startnum += secondarynum;
+        secondarynum = holder;
+    }
+    return secondarynum;
+}
+
+var y = 6;
+console.log(fibo(y));
 
 // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
 
