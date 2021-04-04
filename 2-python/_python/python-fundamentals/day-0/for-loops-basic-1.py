6b91d68c629f0a886c4ea82450e6ebd3ca072e5b
djdembeck <dembeck.david@protonmail.com>
2021-04-04 14:03:44 -0500

    partially completed

--- 2-python/_python/python-fundamentals/day-0/for-loops-basic-1.py
+++ 2-python/_python/python-fundamentals/day-0/for-loops-basic-1.py
@@ -1,11 +1,19 @@
 # Basic - Print all integers from 0 to 150.
-
+for x in range(151):
+	print(x)
 
 # Multiples of Five - Print all the multiples of 5 from 5 to 1,000
-
+for x in range(5, 1001, 5):
+	print(x)
 
 # Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
-
+for x in range(1, 101, 1):
+	if x % 10 == 0:
+		print("Coding Dojo")
+	elif x % 5 == 0:
+		print("Coding")
+	else:
+		print(x)
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 
