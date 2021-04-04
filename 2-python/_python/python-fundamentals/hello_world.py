b263b55294eb4084fd6d27fa08678bd3c457e7f7
djdembeck <dembeck.david@protonmail.com>
2021-04-04 00:09:25 -0500

    hello world assignment

--- 2-python/_python/python-fundamentals/hello_world.py
+++ 2-python/_python/python-fundamentals/hello_world.py
@@ -1,4 +1,15 @@
-x = "Hello Python"
-print(x)
-y = 42
-print(y)
+# 1. TASK: print "Hello World"
+print("Hello World!")
+# 2. print "Hello David!" with the name in a variable
+name = "David"
+print("Hello", name)	# with a comma
+print("Hello " + name)	# with a +
+# 3. print "Hello 25!" with the number in a variable
+num = 25
+print("Hello", num)	# with a comma
+print("Hello " + num )	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "chicken"
+fave_food2 = "rice"
+print("I love to eat {} and {}".format(fave_food1, fave_food2)) # with .format()
+print("I love to eat fave_food1 and fave_food2") # with an f string
