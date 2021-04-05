d2ce9604063320bac8eb97964e2b978930743473
djdembeck <dembeck.david@protonmail.com>
2021-04-05 00:42:33 -0500

    finish next

--- 2-python/_python/python-fundamentals/day-0/for-loops-basic-1.py
+++ 2-python/_python/python-fundamentals/day-0/for-loops-basic-1.py
@@ -16,7 +16,10 @@ for x in range(1, 101, 1):
 		print(x)
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
-
+sum = 0
+for x in range(1, 500001, 2):
+	sum += x
+print(sum)
 
 # Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 
