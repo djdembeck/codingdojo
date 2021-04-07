f3b34091643d11437c467bc36b2170aa06b75332
djdembeck <dembeck.david@protonmail.com>
2021-04-07 16:52:02 -0500

    moar experimenting

--- 2-python/flask/flask_fundamentals/day-3/hello_flask/hello.py
+++ 2-python/flask/flask_fundamentals/day-3/hello_flask/hello.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 ### Routes
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return render_template('index.html')
 
 @app.route('/dojo')
 def dojo():
@@ -23,7 +22,8 @@ def repeat(num, word):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "Sorry! No response. Try again."
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 ### End routes
 
