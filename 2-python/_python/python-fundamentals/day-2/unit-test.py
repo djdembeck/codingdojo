5d9dc962d0216bba999c05f5822658c01bbefb1c
djdembeck <dembeck.david@protonmail.com>
2021-04-07 00:16:20 -0500

    setup next problems

--- 2-python/_python/python-fundamentals/day-2/unit-test.py
+++ 2-python/_python/python-fundamentals/day-2/unit-test.py
@@ -1,7 +1,6 @@
-# import the python testing framework
 import unittest
-# our "unit"
-# this is what we are running our test on
+
+### reverseList - Write a function that reverses the values in the list (without creating a temporary array).
 def reverseList(arr):
 	replace = len(arr) - 1
 	for i in range(0, len(arr) - 2, 1):
@@ -10,10 +9,7 @@ def reverseList(arr):
 
 	return arr
 
-# our "unit tests"
-# initialized by creating a class that inherits from unittest.TestCase
 class reverseListTests(unittest.TestCase):
-	# each method in this class is a test to be run
 	def testOne(self):
 		self.assertNotEqual(reverseList([37,2,1,-9]), ([37,2,1,-9]))
 		self.assertEqual(reverseList([37,2,1,-9]), ([-9, 1, 2, 37]))
@@ -21,12 +17,16 @@ class reverseListTests(unittest.TestCase):
 		self.assertIsNot(reverseList([37,2,1,-9]), "")
 		self.assertIsNotNone(reverseList([37,2,1,-9]))
 	def setUp(self):
-		# add the setUp tasks
 		print("running setUp")
-	# any task you want run after the tests are executed, put them in the tearDown method
 	def tearDown(self):
-		# add the tearDown tasks
 		print("running tearDown tasks")
 if __name__ == '__main__':
-    unittest.main() # this runs our tests
+    unittest.main()
 
+### isPalindrome - Write a function that checks whether the given word is a palindrome (a word that spells the same backward).
+
+### coins - Write a function that determines how many quarters, dimes, nickels, and pennies to give to a customer for a change where you minimize the number of coins you give out.
+
+### BONUS - factorial - Write a recursive function that returns the factorial of a given number. Remember that the factorial of a number is the product of all the numbers between 1 and the given number (eg. 4! = 4*3*2*1).
+
+### BONUS - fibonacci - Write a recursive function that accepts a number, n, and returns the nth Fibonacci number from the sequence. The first two Fibonacci numbers are 0 and 1. Every number after that is calculated by adding the previous 2 numbers from the sequence. (i.e. 0, 1, 1, 2, 3, 5, 8, 13, 21 ...)
