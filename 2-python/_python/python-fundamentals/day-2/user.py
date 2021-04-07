39b6de83dbc6b037008fe5eb72fdf447105a8f73
djdembeck <dembeck.david@protonmail.com>
2021-04-06 22:14:51 -0500

    start

---
+++ 2-python/_python/python-fundamentals/day-2/user.py
@@ -0,0 +1,6 @@
+# For this assignment, we'll add some functionality to the User class:
+
+# make_withdrawal(self, amount) - have this method decrease the user's balance by the amount specified
+# display_user_balance(self) - have this method print the user's name and account balance to the terminal
+# eg. "User: Guido van Rossum, Balance: $150
+# BONUS: transfer_money(self, other_user, amount) - have this method decrease the user's balance by the amount and add that amount to other other_user's balance
