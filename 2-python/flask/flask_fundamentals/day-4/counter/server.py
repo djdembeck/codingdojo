62c9801435829e33989639dc47d4a236fd9e149c
djdembeck <dembeck.david@protonmail.com>
2021-04-09 14:28:38 -0500

    basic counter done

--- 2-python/flask/flask_fundamentals/day-4/counter/server.py
+++ 2-python/flask/flask_fundamentals/day-4/counter/server.py
@@ -4,7 +4,26 @@ app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	if 'add' in session:
+		session.pop('add')
+	elif 'num_requests' in session:
+		print(session['num_requests'])
+		session['num_requests'] += 1
+	else:
+		session['num_requests'] = 1
+
+	return render_template("index.html", num_requests=session['num_requests'])
+
+@app.route('/reset', methods=['POST'])
+def reset():
+	session.clear()
+	return redirect ("/")
+
+@app.route('/add', methods=['POST'])
+def add():
+	session['num_requests'] += 2
+	session['add'] = True
+	return redirect ("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
