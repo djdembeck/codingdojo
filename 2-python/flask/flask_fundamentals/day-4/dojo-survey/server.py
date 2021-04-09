b048f8db6720e91f3987e4b6f8ea5a2b02004500
djdembeck <dembeck.david@protonmail.com>
2021-04-08 23:37:59 -0500

    session persistence

--- 2-python/flask/flask_fundamentals/day-4/dojo-survey/server.py
+++ 2-python/flask/flask_fundamentals/day-4/dojo-survey/server.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
 
-# our index route will handle rendering our form
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -10,23 +10,29 @@ def index():
 def survey_result():
 	print("Got Post Info")
 	print(request.form)
-	form_name = request.form['name']
-	form_location = request.form['location']
+	session['form_name'] = request.form['name']
+	session['form_location'] = request.form['location']
+	# These don't need to be stored, as they are combined below in 'final_fav_langs'
 	form_fav_lang_py = request.form.get('python')
 	form_fav_lang_mern = request.form.get('mern')
 	form_fav_lang_java = request.form.get('java')
 	form_fav_lang_csharp = request.form.get('csharp')
-	form_enjoyable = request.form.get('answer')
-	form_comment = request.form['comments']
+	session['form_enjoyable'] = request.form.get('answer')
+	session['form_comment'] = request.form['comments']
 
 	fav_langs = {"Python": form_fav_lang_py, "MERN": form_fav_lang_mern, "Java": form_fav_lang_java, "C#": form_fav_lang_csharp}
 	websel = []
 	for selection in fav_langs:
 		if fav_langs[selection] == "on":
 			websel.append(selection)
-	final_fav_langs = ', '.join(websel)
+	session['final_fav_langs'] = ', '.join(websel)
 
-	return render_template("show.html", form_name=form_name, form_location=form_location, final_fav_langs=final_fav_langs, form_enjoyable=form_enjoyable, form_comment=form_comment)
+	return redirect ("/show")
+
+@app.route("/show")
+def show_result():
+	print(request.form)
+	return render_template("show.html", form_name=session['form_name'], form_location=session['form_location'], final_fav_langs=session['final_fav_langs'], form_enjoyable=session['form_enjoyable'], form_comment=session['form_comment'])
 
 if __name__ == "__main__":
     app.run(debug=True)
