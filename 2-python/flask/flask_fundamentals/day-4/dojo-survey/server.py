e26bebd5799193ede42d28ac996b41c1dcf31982
djdembeck <dembeck.david@protonmail.com>
2021-04-08 23:19:15 -0500

    finish challenges/project

--- 2-python/flask/flask_fundamentals/day-4/dojo-survey/server.py
+++ 2-python/flask/flask_fundamentals/day-4/dojo-survey/server.py
@@ -6,13 +6,27 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/users', methods=['POST'])
-def create_user():
+@app.route('/result', methods=['POST'])
+def survey_result():
 	print("Got Post Info")
 	print(request.form)
-	name_from_form = request.form['name']
-	email_from_form = request.form['email']
-	return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+	form_name = request.form['name']
+	form_location = request.form['location']
+	form_fav_lang_py = request.form.get('python')
+	form_fav_lang_mern = request.form.get('mern')
+	form_fav_lang_java = request.form.get('java')
+	form_fav_lang_csharp = request.form.get('csharp')
+	form_enjoyable = request.form.get('answer')
+	form_comment = request.form['comments']
+
+	fav_langs = {"Python": form_fav_lang_py, "MERN": form_fav_lang_mern, "Java": form_fav_lang_java, "C#": form_fav_lang_csharp}
+	websel = []
+	for selection in fav_langs:
+		if fav_langs[selection] == "on":
+			websel.append(selection)
+	final_fav_langs = ', '.join(websel)
+
+	return render_template("show.html", form_name=form_name, form_location=form_location, final_fav_langs=final_fav_langs, form_enjoyable=form_enjoyable, form_comment=form_comment)
 
 if __name__ == "__main__":
     app.run(debug=True)
