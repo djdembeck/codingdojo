341d17d592745ca5eff41b47f1251bf06637cd52
djdembeck <dembeck.david@protonmail.com>
2021-04-10 20:41:16 -0500

    initial setup, still needs lots of work

---
+++ 2-python/_python/OOP/zoo/setup.py
@@ -0,0 +1,36 @@
+# Imagine a game where you can create a zoo and start raising different types of animals. Say that for each zoo you create can have several different animals. Start by creating an Animal class, and then at least 3 specific animal classes that inherit from Animal. (Maybe lions and tigers and bears, oh my!) Each animal should at least have a name, an age, a health level, and happiness level. The Animal class should have a display_info method that shows the animal's name, health, and happiness. It should also have a feed method that increases health and happiness by 10.
+
+# In at least one of the Animal child classes you've created, add at least one unique attribute. Give each animal different default levels of health and happiness. The animals should also respond to the feed method with varying levels of changes to health and happiness.
+
+# Once you've tested out your different animals and feel more comfortable with inheritance, create a Zoo class to help manage all your animals.
+
+    # def add_lion(self, name):
+    #     self.animals.append( Lion(name) )
+    # def add_tiger(self, name):
+    #     self.animals.append( Tiger(name) )
+
+class Animal:
+	def __init__(self):
+		pass
+	return self
+	def set_age(self):
+		pass
+		# randint
+	def set_health(self):
+		pass
+		# use random data to set health between 1-10
+	def set_happiness(self):
+		pass
+		# randint 1-10
+	def display_info(self, name):
+		pass
+	def feed(self, type):
+		pass
+		# increases health and happiness by 10.
+
+class Lion(Animal):
+	def __init__(self):
+		pass
+	return self
+	def set_name(self, name):
+		pass
