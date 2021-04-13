de7461dc0d592785fd358edf1c6cfac1eb742f9a
djdembeck <dembeck.david@protonmail.com>
2021-04-12 23:01:28 -0500

    main working

--- 2-python/django/django-fundamentals/day-1/great_number_game/main/views.py
+++ 2-python/django/django-fundamentals/day-1/great_number_game/main/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render, HttpResponse, redirect
+import random
 
 def index(request):
+	if not 'rand_result' in request.session:
+		request.session['rand_result'] = random.randrange(1, 100)
+		print(request.session['rand_result'])
 	return render(request, "index.html")
+
+def take_a_guess(request):
+	if not 'rand_result' in request.session:
+		request.session['guess'] = ""
+	if int(request.POST['guess']) > request.session['rand_result']:
+		request.session['guess'] = "high"
+		print("Booo, too high")
+	elif int(request.POST['guess']) < request.session['rand_result']:
+		print("Booo, too low")
+		request.session['guess'] = "low"
+	elif int(request.POST['guess']) == request.session['rand_result']:
+		print(f"Nice guess ya fucking wanker: {request.session['rand_result']}")
+		request.session['guess'] = "right"
+	return redirect ("/")
