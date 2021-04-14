e5aaf66718a6b3bd64cfbf2470da4ce59aea3472
djdembeck <dembeck.david@protonmail.com>
2021-04-14 00:49:26 -0500

    code cleanup

--- 2-python/django/django-orm/day-3/single_model_orm/users_app/views.py
+++ 2-python/django/django-orm/day-3/single_model_orm/users_app/views.py
@@ -2,27 +2,15 @@ from django.shortcuts import render, redirect
 from .models import Users
 
 def index(request):
-	# first_name = request.POST['first_name']
-	# last_name = request.POST['last_name']
-	# email_address = request.POST['email']
-	# age = request.POST['age']
-
 	context = {
 		"all_the_users": Users.objects.all()
 	}
 
-	# new_user = Users.objects.create(first_name=first_name,last_name=last_name,email_address=email_address,age=age)
-	# new_user.save()
 	return render(request, "index.html", context)
 
 def add_user(request):
 	new_user = Users.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email_address=request.POST['email_address'],age=int(request.POST['age']))
 	new_user.save()
-	print(request.POST['first_name'])
-	print(request.POST['last_name'])
-	print(request.POST['email_address'])
-	print(int(request.POST['age']))
-	print(Users.objects.all())
 	return redirect('/')
 
 def remove_last(request):
