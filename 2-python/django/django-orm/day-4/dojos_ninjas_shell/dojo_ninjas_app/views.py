520c1fc819f813857d8c0a41018e0b634ed98c0f
djdembeck <dembeck.david@protonmail.com>
2021-04-16 15:13:07 -0500

    finish assignment

--- 2-python/django/django-orm/day-4/dojos_ninjas_shell/dojo_ninjas_app/views.py
+++ 2-python/django/django-orm/day-4/dojos_ninjas_shell/dojo_ninjas_app/views.py
@@ -4,17 +4,19 @@ from .models import Dojo, Ninja
 # Create your views here.
 def index(request):
 	context = {
-		"all_the_dojos": Dojo.objects.all()
+		"all_the_dojos": Dojo.objects.all(),
 	}
 
 	return render(request, "index.html", context)
 
 def add_dojo(request):
 	# Dojo.objects.create(name="Best", city="Dallas", state="TX")
-	new_user = Users.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],email_address=request.POST['email_address'],age=int(request.POST['age']))
-	new_user.save()
+	new_dojo = Dojo.objects.create(name=request.POST['dojo_name'],city=request.POST['city'],state=request.POST['state'])
+	new_dojo.save()
 	return redirect('/')
 
 def add_ninja(request):
 	# Ninja.objects.create(first_name="Apollo", last_name="Zuesson", location=Dojo.objects.get(id=5))
+	new_ninja = Ninja.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],location = Dojo.objects.get(id=request.POST['dojo_id']))
+	new_ninja.save()
 	return redirect('/')
