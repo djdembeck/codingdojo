feb7ce92c91f5bc94b83ca33f0910d631063d11c
djdembeck <dembeck.david@protonmail.com>
2021-04-19 14:57:34 -0500

    add show update function

--- 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/urls.py
+++ 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
+	path('', views.root),
+	path('shows', views.index),
 	path('shows/<int:id>', views.show_details),
     path('shows/new', views.new_show),
 	path('shows/create', views.add_show),
 	path('shows/<int:id>/delete', views.del_show),
 	path('shows/<int:id>/edit', views.edit_show),
+	path('shows/<int:id>/update', views.update_show),
 ]
--- 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/views.py
+++ 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Show
 
 # Create your views here.
+def root(request):
+	return redirect('/shows')
+
+def index(request):
+	context = {
+		"all_shows": Show.objects.all(),
+	}
+	return render(request, "index.html", context)
 
 def new_show(request):
-
 	return render(request, "create_show.html")
 
 def add_show(request):
@@ -24,6 +31,15 @@ def edit_show(request, id):
 		"this_show": Show.objects.get(id=id),
 	}
 	return render(request, "edit_show.html", context)
+
+def update_show(request, id):
+	show_to_update = Show.objects.get(id=id)
+	show_to_update.title = request.POST['title']
+	show_to_update.network = request.POST['network']
+	show_to_update.release_date = request.POST['release_date']
+	show_to_update.desc = request.POST['description']
+	show_to_update.save()
+	return redirect(f'/shows/{id}')
 
 def del_show(request, id):
 	show = Show.objects.get(id=id)
