5a04b15a8dbcd76c644505440069ecddf43d057d
djdembeck <dembeck.david@protonmail.com>
2021-04-19 16:53:32 -0500

    add validations

--- 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/models.py
+++ 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from datetime import date
+
+class ShowManager(models.Manager):
+	def basic_validator(self, post_data):
+		errors = {}
+		if len(post_data['title']) < 5:
+			errors["title"] = "Show title should be at least 5 characters"
+		if 0 < len(post_data['description']) < 25:
+			errors["description"] = "Show description can be either empty or more than 25 characters"
+		if len(post_data['network']) < 2:
+			errors["network"] = "Network should be more than 2 characters"
+		if post_data['release_date'] > str(date.today()):
+			errors["network"] = "Date must be in the past"
+		return errors
 
 class Show(models.Model):
 	title = models.CharField(max_length=255)
@@ -7,3 +21,4 @@ class Show(models.Model):
 	desc = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = ShowManager()
--- 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/views.py
+++ 2-python/django/django-fullstack/day-0/semi_restful_tv_shows/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 # Create your views here.
 def root(request):
@@ -15,8 +16,15 @@ def new_show(request):
 	return render(request, "create_show.html")
 
 def add_show(request):
-	new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['description'])
-	new_show.save()
+	errors = Show.objects.basic_validator(request.POST)
+	if len(errors) > 0:
+		for k, v in errors.items():
+			messages.error(request, v)
+		return redirect('/shows/new')
+	else:
+		new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], desc=request.POST['description'])
+		new_show.save()
+		messages.success(request, "Show successfully added")
 
 	return redirect(f'/shows/{new_show.id}')
 
@@ -33,12 +41,20 @@ def edit_show(request, id):
 	return render(request, "edit_show.html", context)
 
 def update_show(request, id):
-	show_to_update = Show.objects.get(id=id)
-	show_to_update.title = request.POST['title']
-	show_to_update.network = request.POST['network']
-	show_to_update.release_date = request.POST['release_date']
-	show_to_update.desc = request.POST['description']
-	show_to_update.save()
+	errors = Show.objects.basic_validator(request.POST)
+	if len(errors) > 0:
+		for k, v in errors.items():
+			messages.error(request, v)
+		return redirect(f'/shows/{id}/edit')
+	else:
+		show_to_update = Show.objects.get(id=id)
+		show_to_update.title = request.POST['title']
+		show_to_update.network = request.POST['network']
+		show_to_update.release_date = request.POST['release_date']
+		show_to_update.desc = request.POST['description']
+		show_to_update.save()
+		messages.success(request, "Show successfully updated")
+
 	return redirect(f'/shows/{id}')
 
 def del_show(request, id):
