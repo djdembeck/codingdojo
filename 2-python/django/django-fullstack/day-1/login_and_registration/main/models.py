f2eec554488cdbf31405838482073eec474ae7b2
djdembeck <dembeck.david@protonmail.com>
2021-04-20 01:12:46 -0500

    fix validations

--- 2-python/django/django-fullstack/day-1/login_and_registration/main/models.py
+++ 2-python/django/django-fullstack/day-1/login_and_registration/main/models.py
@@ -1,4 +1,37 @@
 from django.db import models
+import re
+
+class UserManager(models.Manager):
+	def basic_validator(self, post_data):
+		errors = {}
+
+		# first name validations
+		if len(post_data['first_name']) < 3:
+			errors["first_name"] = "First name should have more than 3 characters"
+		if len(post_data['first_name']) > 50:
+			errors["first_name"] = "First name should NOT have more than 50 characters"
+
+		# last name validations
+		if len(post_data['last_name']) < 3:
+			errors["last__name"] = "Last name should have more ethan 3 characters"
+		if len(post_data['last_name']) > 50:
+			errors["last_name"] = "First name should NOT have more than 50 characters"
+
+		# Email validations
+		EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+		if not EMAIL_REGEX.match(post_data['register_email']):
+			errors["register_email"] = "Invalid email address!"
+
+		# Password validations
+		if len(post_data['register_password']) < 8:
+			errors['register_password'] = "Password is too short!"
+		if len(post_data['register_password']) > 256:
+			errors['register_password'] = "Password is too long!"
+		if post_data['register_password'] != post_data['register_confirm_password']:
+			errors['register_confirm_password'] = "Passwords do not match"
+
+		return errors
 
 # Create your models here.
 class User(models.Model):
@@ -8,3 +41,4 @@ class User(models.Model):
 	password  = models.CharField(max_length=512)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = UserManager()
--- 2-python/django/django-fullstack/day-1/login_and_registration/main/views.py
+++ 2-python/django/django-fullstack/day-1/login_and_registration/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import User
+from django.contrib import messages
 import bcrypt
 
 # Create your views here.
@@ -7,10 +8,17 @@ def index(request):
 	return render(request, "index.html")
 
 def register(request):
-	password = request.POST['register_password']
-	pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-	new_user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['register_email'], password=pw_hash)
-	new_user.save()
+	errors = User.objects.basic_validator(request.POST)
+	if len(errors) > 0:
+		for k, v in errors.items():
+			messages.error(request, v)
+		return redirect('/')
+	else:
+		password = request.POST['register_password']
+		pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+		new_user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['register_email'], password=pw_hash)
+		new_user.save()
+		
 	return redirect('/success')
 
 def success(request):
