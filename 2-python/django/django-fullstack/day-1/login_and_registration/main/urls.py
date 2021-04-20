0f312ce222accaf434b3d83cf8bd96e05136b3f1
djdembeck <dembeck.david@protonmail.com>
2021-04-20 00:22:42 -0500

    fixup register method

--- 2-python/django/django-fullstack/day-1/login_and_registration/main/urls.py
+++ 2-python/django/django-fullstack/day-1/login_and_registration/main/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path('', views.index),
+	path('register', views.register),
+	path('success', views.success),
 ]
--- 2-python/django/django-fullstack/day-1/login_and_registration/main/views.py
+++ 2-python/django/django-fullstack/day-1/login_and_registration/main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render,redirect
+from .models import User
+import bcrypt
 
 # Create your views here.
 def index(request):
 	return render(request, "index.html")
+
+def register(request):
+	password = request.POST['register_password']
+	pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+	new_user = User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['register_email'], password=pw_hash)
+	new_user.save()
+	return redirect('/success')
+
+def success(request):
+	context = {
+		"this_user": User.objects.last()
+	}
+	return render(request, "success.html", context)
