ab9b036451e7d983a92ce2ca74a0b95d40eb8827
djdembeck <dembeck.david@protonmail.com>
2021-04-21 09:41:58 -0500

    work so far

--- 2-python/django/django-fullstack/day-2/algo-practice.js
+++ 2-python/django/django-fullstack/day-2/algo-practice.js
@@ -1,60 +1,116 @@
 class ListNode {
-    constructor(value) {
-      this.value = value;
-      this.next = null;
-    }
-  }
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
 
 // a queue! first in, first out
 // we add to the head of a queue but remove from the tail
 
 class SLLQueue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-    // enqueue(value) - adds the given value to the queue (at the head)
-    enqueue(value) {
-        
-    }
+	constructor() {
+		this.head = null;
+		this.tail = null;
+	}
 
-    // dequeue() - removes the front (tail) value from queue and returns it
-    dequeue() {
-        
-    }
+	// enqueue(value) - adds the given value to the queue (at the head)
+	enqueue(value) {
+		var new_node = new ListNode(value);
 
-    // front() - returns the front (tail) value without removing it
-    front() {
+		if (this.head == null) {
+			this.head = new_node;
+			this.tail = new_node;
+		}
+		
+		else {
+			new_node.next = this.head;
+			this.head = new_node;
+		}
+	}
 
-    }
+	// dequeue() - removes the front (tail) value from queue and returns it
+	dequeue() {
+		if (this.head == null) {
+			return null;
+		}
 
-    // contains(target) - returns true if the target value is in the queue,
-    // false if not
-    contains(target) {
+		if (this.head == this.tail) {
+			var temp = this.head;
+			this.head = null;
+			this.tail = null;
+			return temp.value
+		}
 
-    }
+		var runner = this.head;
+		temp = this.tail;
+		if (this.head == null || this.tail == null) {
+			return undefined;
+		}
+		// 2nd to last's next will be tail
+		while (runner.next.next != null) {
+			runner = runner.next;
+		}
+		runner.next = null;
+		runner = this.tail;
 
-    // isEmpty() - returns true if the queue is empty, false otherwise
-    isEmpty() {
+		return temp.value;
+	}
 
-    }
+	// front() - returns the front (tail) value without removing it
+	front() {
+		if (this.head == null) {
+			return null;
+		}
 
-    // size() - returns the size of the queue
-    size() {
+		return this.tail.value
+	}
 
-    }
+	// contains(target) - returns true if the target value is in the queue,
+	// false if not
+	contains(target) {
+		var runner = this.head;
 
-    // compare(other_queue) - return true if this queue and the other_queue
-    // have the same values in the same order and false otherwise
-    // do not modify either queue in this instance
-    // remember:
-    // var x = new ListNode(7);
-    // var y = new ListNode(7);
-    // x == y? false
-    // x.value == y.value? true
-    compare(other_queue) {
+		while (runner != null) {
+			if (runner.value == target) {
+				return true;
+			}
+			runner = runner.next;
+		}
+		return false;
+	}
 
-    }
+	// isEmpty() - returns true if the stack is empty, false otherwise
+	isEmpty() {
+		if (this.head == null) {
+			return true;
+		}
+		return false;
+	}
+
+	// size() - returns the size of the stack
+	size() {
+		var runner = this.head;
+		var counter = 0;
+
+		while (runner != null) {
+			counter += 1;
+			runner = runner.next;
+		}
+		return counter;
+	}
+
+	// compare(other_queue) - return true if this queue and the other_queue
+	// have the same values in the same order and false otherwise
+	// do not modify either queue in this instance
+	// remember:
+	// var x = new ListNode(7);
+	// var y = new ListNode(7);
+	// x == y? false
+	// x.value == y.value? true
+	compare(other_queue) {
+
+	}
 }
 
 var x = new SLLQueue();
@@ -66,6 +122,8 @@ var a = new SLLQueue();
 a.enqueue(7);
 a.enqueue(3);
 a.enqueue(11);
+
+console.log(a.dequeue())
 
 var b = new SLLQueue();
 b.enqueue(7);
