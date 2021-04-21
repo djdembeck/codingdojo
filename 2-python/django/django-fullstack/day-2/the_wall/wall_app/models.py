831655c625ae05cc14cd3fe3db7f4d2bc7ae52d8
djdembeck <dembeck.david@protonmail.com>
2021-04-21 15:25:03 -0500

    update models

--- 2-python/django/django-fullstack/day-2/the_wall/wall_app/models.py
+++ 2-python/django/django-fullstack/day-2/the_wall/wall_app/models.py
@@ -3,17 +3,14 @@ from main.models import User
 
 # Create your models here.
 class Post(models.Model):
-	user_id = models.IntegerField()
+	user = models.ForeignKey(User, related_name="posts", on_delete = models.CASCADE)
 	message = models.TextField()
-	models.ForeignKey(User, related_name="posts", on_delete = models.CASCADE)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-	message_id = models.IntegerField()
-	models.ForeignKey(Post, related_name="comments", on_delete = models.CASCADE)
-	models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
-	user_id = models.IntegerField()
+	message = models.ForeignKey(Post, related_name="comments", on_delete = models.CASCADE)
+	user = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
 	comment = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
