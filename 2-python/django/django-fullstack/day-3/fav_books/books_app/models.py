0352f506b14eb474771bf8264fe85aaa9344f8b6
djdembeck <dembeck.david@protonmail.com>
2021-04-21 23:17:31 -0500

    basic templates ready

--- 2-python/django/django-fullstack/day-3/fav_books/books_app/models.py
+++ 2-python/django/django-fullstack/day-3/fav_books/books_app/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from login.models import User
 
 # Create your models here.
-class Post(models.Model):
-	user = models.ForeignKey(User, related_name="books", on_delete = models.CASCADE)
+class Book(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
+	uploaded_by = models.ForeignKey(User, related_name="books_uploaded", on_delete = models.CASCADE)
+	users_who_like = models.ManyToManyField(User, related_name="liked_books")
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
