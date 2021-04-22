d5d9d7236752b9fb0f28164a7a01995115eac725
djdembeck <dembeck.david@protonmail.com>
2021-04-21 22:04:48 -0500

    add deletion validations

--- 2-python/django/django-fullstack/day-2/the_wall/wall_app/models.py
+++ 2-python/django/django-fullstack/day-2/the_wall/wall_app/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 from main.models import User
+from datetime import datetime,timezone,timedelta
+
+
+class PostManager(models.Manager):
+	
+	def deletion_validator(self, content_type, post_id, user_id):
+		errors = {}
+		# Check what type we need to check against: post or comment
+		if content_type == "post":
+			check_poster = Post.objects.get(id=post_id)
+		elif content_type == "comment":
+			check_poster = Comment.objects.get(id=post_id)
+		
+		# Ensure request comes from the user id that created the content
+		if check_poster.user.id != user_id:
+			errors['failed_to_delete'] = "You cannot delete an item you didn't create"
+
+		now = datetime.now(timezone.utc)
+		# Check if post was made in the last 30 minutes
+		if not now-timedelta(minutes=30) <= check_poster.created_at <= now:
+			errors['too_old_to_delete'] = "You cannot delete an item older than 30 minutes"
+
+		return errors
 
 # Create your models here.
 class Post(models.Model):
@@ -7,6 +30,7 @@ class Post(models.Model):
 	message = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = PostManager()
 
 class Comment(models.Model):
 	message = models.ForeignKey(Post, related_name="comments", on_delete = models.CASCADE)
@@ -14,3 +38,4 @@ class Comment(models.Model):
 	comment = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+	objects = PostManager()
--- 2-python/django/django-fullstack/day-2/the_wall/wall_app/views.py
+++ 2-python/django/django-fullstack/day-2/the_wall/wall_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from main.models import User
 from .models import Post, Comment
+from django.contrib import messages
 
 # Create your views here.
 def feed(request):
+	# Don't even process if user isn't signed in
 	if 'userid' not in request.session:
 		return redirect('/')
 
@@ -14,17 +16,35 @@ def feed(request):
 	return render(request, "feed.html", context)
 
 def make_post(request):
+	# Don't even process if user isn't signed in
+	if 'userid' not in request.session:
+		return redirect('/')
+
 	Post.objects.create(message=request.POST['post_contents'], user=User.objects.get(id=request.session['userid']))
 	return redirect ('/wall')
 
 def make_comment(request, parent_id):
+	# Don't even process if user isn't signed in
+	if 'userid' not in request.session:
+		return redirect('/')
+
 	Comment.objects.create(comment=request.POST['comment_contents'], user=User.objects.get(id=request.session['userid']), message=Post.objects.get(id=parent_id))
 	return redirect('/wall')
 
 def delete(request, content_type, id):
-	if content_type == "post":
-		Post.objects.get(id=id).delete()
-	if content_type == "comment":
-		Comment.objects.get(id=id).delete()
+	# Don't even process if user isn't signed in
+	if 'userid' not in request.session:
+		return redirect('/')
 
-	return redirect('/wall')
+	errors = Post.objects.deletion_validator(content_type, id, request.session['userid'])
+	if len(errors) > 0:
+		for k, v in errors.items():
+			messages.error(request, v)
+		return redirect('/wall')
+	else:
+		if content_type == "post":
+			Post.objects.get(id=id).delete()
+		if content_type == "comment":
+			Comment.objects.get(id=id).delete()
+
+		return redirect('/wall')
