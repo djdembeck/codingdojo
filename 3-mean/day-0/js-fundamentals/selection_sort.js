35c1f76e4bff6f530a0f2778b1677d7d8a4aab16
djdembeck <dembeck.david@protonmail.com>
2021-05-03 10:15:26 -0500

    fix

--- 3-mean/day-0/js-fundamentals/selection_sort.js
+++ 3-mean/day-0/js-fundamentals/selection_sort.js
@@ -1,10 +1,14 @@
 function selectionSort(arr) {
 	for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr.length; j++) {
-			if (arr[j] > arr[i]) {
-				[arr[i], arr[j]] = [arr[j], arr[i]];
+		var marker = i;
+		var lowest = arr[i];
+		for (let j = i; j < arr.length; j++) {
+			if (arr[j] < lowest) {
+				lowest = arr[j]
+				marker = j;
 			}
 		}
+		[arr[marker], arr[i]] = [arr[i], arr[marker]];
 	}
 	return arr;
 }
