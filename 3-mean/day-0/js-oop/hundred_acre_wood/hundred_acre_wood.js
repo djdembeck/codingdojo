dadb34dfa13c32064c47f8db0e40d6c115ecccd6
djdembeck <dembeck.david@protonmail.com>
2021-05-03 16:47:47 -0500

    add next steps

--- 3-mean/day-0/js-oop/hundred_acre_wood/hundred_acre_wood.js
+++ 3-mean/day-0/js-oop/hundred_acre_wood/hundred_acre_wood.js
@@ -1,14 +1,69 @@
-var bees = {character: "Bees"};
-var robin = {character: "Christopher Robin"};
-var eeyore = {character: "Eeyore"};
-var gopher = {character: "Gopher"};
-var heff = {character: "Heff"};
-var kanga = {character: "Kanga"};
-var owl = {character: "Owl"};
-var piglet = {character: "Piglet"};
-var pooh = {character: "Winnie the Pooh"};
-var rabbit = {character: "Rabbit"};
-var tigger = {character: "Tigger"};
+var bees = {
+	character: "Bees",
+	greet: function(){
+		console.log("Bzzzzzzzzzzzzzzz")
+	}
+};
+var robin = {
+	character: "Christopher Robin",
+	greet: function(){
+		console.log("You’re braver than you believe, stronger than you seem and smarter than you think.")
+	}
+};
+var eeyore = {
+	character: "Eeyore",
+	greet: function(){
+		console.log("After all, one can’t complain. I have my friends.")
+	}
+};
+var gopher = {
+	character: "Gopher",
+	greet: function(){
+		console.log("Does a gopher have emotions?")
+	}
+};
+var heff = {
+	character: "Heff",
+	greet: function(){
+		console.log("Why me?")
+	}
+};
+var kanga = {
+	character: "Kanga",
+	greet: function(){
+		console.log("How can a loving god cause such misery?")
+	}
+};
+var owl = {
+	character: "Owl",
+	greet: function(){
+		console.log("If the string breaks, then we try another piece of string.")
+	}
+};
+var piglet = {
+	character: "Piglet",
+	greet: function(){
+		console.log("Oh d-d-d-d-dear! I wasn't expecting company!")
+	}
+};
+var pooh = {
+	character: "Winnie the Pooh",
+	greet: function(){
+		console.log("Oh bother")
+	}
+};
+var rabbit = {
+	character: "Rabbit",
+	greet: function(){
+		console.log("Tiggers never go on being sad.")
+	}
+};
+var tigger = {
+	character: "Tigger",
+	greet: function(){
+		console.log("The wonderful thing about tiggers is tiggers are wonderful things")
+	}
+};
 
 var player = {
 	location: tigger
@@ -64,6 +119,7 @@ function move(direction) {
 	if (player.location[direction]) {
 		player.location = player.location[direction]
 		console.log(`You are now at ${player.location.character}'s house`)
+		player.location.greet();
 	} else {
 		console.log("Invalid direction")
 	}
