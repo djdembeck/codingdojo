336f329324d7aa4d1bf0dc0067d7884d8a820350
djdembeck <dembeck.david@protonmail.com>
2021-05-06 00:33:39 -0500

    finish deck of cards

--- 3-mean/day-2/deck_of_cards.js
+++ 3-mean/day-2/deck_of_cards.js
@@ -1,7 +1,7 @@
 class Card {
 	constructor () {
-		var suit_arr =	['Hearts', 'Clubs','Diamonds', 'Spades']
-		this.cards = []
+		var suit_arr =	['Hearts', 'Clubs','Diamonds', 'Spades'];
+		this.cards = [];
 		for (var i = 0; i < suit_arr.length; i++) {
 			var card_dict = [
 				{'name': 'Ace', 'value': '1'},
@@ -17,23 +17,82 @@ class Card {
 				{'name': 'Jack', 'value': '11'},
 				{'name': 'Queen', 'value': '12'},
 				{'name': 'King', 'value': '13'}
-			]
+			];
 			for (let j = 0; j < card_dict.length; j++) {
-				let new_entry = card_dict[j]
-				new_entry['suit'] = `${suit_arr[i]}`
-				this.cards.push(new_entry)
+				let new_entry = card_dict[j];
+				new_entry['suit'] = `${suit_arr[i]}`;
+				this.cards.push(new_entry);
 			}
 		}
 	}
+
+	show() {
+		var rand = Math.floor(Math.random() * this.cards.length);
+		var selected = this.cards[rand];
+		return selected;
+	}
 }
 
-var test = new Card()
-console.log(test.cards)
-
 class Deck {
+	all_cards = new Card()
+	constructor () {
+		this.deck = this.all_cards.cards;
+	}
 
+	dealCard() {
+		var rand = Math.floor(Math.random() * this.deck.length);
+		var deal = this.deck[rand];
+		this.deck.splice(rand, 1);
+		return deal;
+	}
+
+	reset() {
+		delete this.deck;
+		var new_deck = new Deck()
+		return new_deck.deck;
+	}
+
+	// Fisher-yates
+	shuffle() {
+		var count = this.deck.length;
+
+		while (count) {
+			var make_random = Math.floor(Math.random() * count--);
+			var current = this.deck[count];
+			this.deck[count] = this.deck[make_random];
+			this.deck[make_random] = current;
+		}
+		return this.deck;
+	}
 }
 
 class Player {
+	all_cards = new Deck()
+	constructor(name) {
+		this.name = name;
+		var count = 5;
+		var partial_hand = [];
+		while (count > 0) {
+			partial_hand.push(this.all_cards.dealCard());
+			count--;
+		}
+		this.hand = partial_hand;
+	}
 
+	discard() {
+		var rand = Math.floor(Math.random() * this.hand.length);
+		this.hand.splice(rand, 1);
+		return this.hand;
+	}
+
+	take() {
+		this.hand.push(this.all_cards.dealCard());
+		return this.hand;
+	}
 }
+
+// var card = new Card()
+// console.log(card.show())
+// var test2 = new Player('Tigress')
+// console.log(test2.take())
+// console.log(test2.discard())
