f7bdb7ef9545cd1fc0e732822039813dbfb873fd
djdembeck <dembeck.david@protonmail.com>
2021-05-07 10:02:15 -0500

    spacing changes

--- 3-mean/morning-algos/0705.js
+++ 3-mean/morning-algos/0705.js
@@ -1,18 +1,18 @@
 function partitionArr(arr, left, right){
 	// Choosing to select pivot from end of array
 	let pivot = arr[right]
-	let i = (left - 1)
+	let i = (left-1)
 
-	for (let j = left; j <= right - 1; j++) {
+	for (let j = left; j <= right-1; j++) {
 		// Swap elements smaller than pivot
 		if (arr[j] < pivot) {
 			// Move left index forward
 			i++;
-			[arr[i],arr[j]] = [arr[j], arr[i]]
+			[arr[i], arr[j]] = [arr[j], arr[i]]
 		}
 	}
 
-	[arr[i + 1],arr[right]] = [arr[right], arr[i + 1]]
+	[arr[i+1], arr[right]] = [arr[right], arr[i+1]]
 	return i + 1;
 }
 
@@ -20,9 +20,9 @@ function quickSort(arr, left, right) {
 	if (left < right) {
 		var partition = partitionArr(arr, left, right)
 		// before pivot point
-		quickSort(arr, left, partition - 1)
+		quickSort(arr, left, partition-1)
 		// after pivot point
-		quickSort(arr, partition + 1, right)
+		quickSort(arr, partition+1, right)
 	}
 	return arr;
 }
