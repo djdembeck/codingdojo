3a236f96234d4a0259dcc6ca7e46bdd096109a09
djdembeck <dembeck.david@protonmail.com>
2021-05-07 11:35:58 -0500

    express counter

---
+++ 3-mean/day-4/counter/server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var session = require("express-session");
+var app = express();
+
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'codingdojorocks'}));  // string for encryption
+
+app.get("/", function (request, response){
+	if (request.session.visits) {
+		request.session.visits += 1;
+	} else {
+		request.session.visits = 1;
+	}
+	console.log(request.session.visits)
+	response.render('index', {visits: request.session.visits});
+})
+
+app.post("/add", function (request, response) {
+	num = request.body['increment']
+	if (num) {
+		request.session.visits += (num - 1)
+	} else {
+		request.session.visits += (2 - 1)
+	}
+	response.redirect('/');
+})
+
+app.post("/reset", function (request, response) {
+	request.session.visits = 0;
+	response.redirect('/');
+})
+
+app.listen(8000, function() {
+	console.log("listening on port 8000");
+})
