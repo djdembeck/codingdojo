ae9813eaa2e5283a89eb880f7f975ea0760c559a
djdembeck <dembeck.david@protonmail.com>
2021-05-12 16:45:52 -0500

    start modularization

---
+++ 3-mean/day-7/quoting_dojo_modular/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var session = require("express-session");
+app.use(session({secret: 'codingdojorocks'}));
+
+app.set('views', __dirname + '/client/views'); 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+const flash = require('express-flash');
+app.use(flash()); 
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/quoting_dojo', {useNewUrlParser: true});
+
+const QuoteSchema = new mongoose.Schema({
+	author: {type: String, required: true, minlength: 6},
+	text: {type: String, required: true, minlength: 20},
+	created_at: Date
+	})
+   // create an object that contains methods for mongoose to interface with MongoDB
+	const Quote = mongoose.model('Quote', QuoteSchema);
+
+require('./server/config/routes.js')(app)
+
+app.listen(8000, function() {
+	console.log("listening on port 8000");
+})
