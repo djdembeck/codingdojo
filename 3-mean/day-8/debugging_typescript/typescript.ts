dbd9a5bcda5c05736628d34be418386efd8a344c
djdembeck <dembeck.david@protonmail.com>
2021-05-13 15:26:25 -0500

    notes

--- 3-mean/day-8/debugging_typescript/typescript.ts
+++ 3-mean/day-8/debugging_typescript/typescript.ts
@@ -3,29 +3,26 @@
 
 // 1. Setting types
 var myString: string|number;
-// I can assign myString like this:
 myString = "Bee stinger";
-// Why is there a problem with this? What can I do to fix this?
 myString = 9;
+// Set type to allow for number
 
 // 2. Setting the types for function parameters
 function sayHello(name: string|number){
 	return `Hello, ${name}!`;
 }
- // This is working great:
 console.log(sayHello("Kermit"));
- // Why isn't this working? I want it to return "Hello, 9!"
 console.log(sayHello(9));
+// Set type to allow for number
 
 // 3. Optional parameters
 function fullName(firstName: string, lastName: string, middleName: string = ""){
 	let fullName = `${firstName} ${middleName} ${lastName}`;
 	return fullName;
 }
- // This works:
 console.log(fullName("Mary", "Moore", "Tyler"));
- // What do I do if someone doesn't have a middle name?
 console.log(fullName("Jimbo", "Jones"));
+// Set default value for middle name
 
 // 4. Interfaces and function parameters
 interface Student {
@@ -46,10 +43,9 @@ const jay = {
 	lastName: "Patel",
 	belts: 4
 }
- // This seems to work fine:
 console.log(graduate(christine));
- // This one has problems:
 console.log(graduate(jay));
+// misspelling of belts
 
 // 5. Classes and function parameters
 class Ninja {
@@ -63,33 +59,27 @@ class Ninja {
 		console.log("Console.log() is my friend.")
 	}
 }
- // This is not making an instance of Ninja, for some reason:
 const shane = new Ninja("Shane", "Thompson");
- // Since I'm having trouble making an instance of Ninja, I decided to do this:
 const turing = new Ninja("Alan", "Turing")
- // Now I'll make a study function, which is a lot like our graduate function from above:
 function study(programmer: Ninja){
 	return `Ready to whiteboard an algorithm, ${programmer.fullName}?`
 }
- // Now this has problems:
 console.log(study(turing));
+// need to set name in class call, fix from dict
 
 // 6. Arrow functions
 var increment = (x:number) => x + 1;
-// This works great:
 console.log(increment(3));
 var square = (x:number) => x * x;
-// This is not showing me what I want:
 console.log(square(4));
-// This is not working:
 var multiply = (x:number, y:number) => x * y;
-// Nor is this working:
 var math = (x:number, y:number) => {  
 	let sum = x + y;
 	let product = x * y;
 	let difference = Math.abs(x-y);
 	return [sum, product, difference];
 }
+// set var types, fix function calls
 
 // 7. Arrow functions and 'this'
 class Elephant {
@@ -103,4 +93,4 @@ setTimeout(babar.birthday, 1000)
 setTimeout(function(){
 	console.log(`Babar's age is ${babar.age}.`)
 	}, 2000)
-// Why didn't babar's age change? Fix this by using an arrow function in the Elephant class.
+// Fix function call from word
