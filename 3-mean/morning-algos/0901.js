5f73995a5c8409f3fe7f546037c703ab99fca16b
djdembeck <dembeck.david@protonmail.com>
2021-05-17 14:58:38 -0500

    finish algo

--- 3-mean/morning-algos/0901.js
+++ 3-mean/morning-algos/0901.js
@@ -1,18 +1,41 @@
 function intersectSortArr(arr1, arr2) {
-	count = 0;
-	newArr = [];
-	for (let i = 0; i < arr1.length; i++) {
-		for (let j = 0 + count; j < arr2.length; j++) {
-			if (arr1[i] == arr2[j]) {
-				newArr.push(arr1[i])
-				count++
-				break
-			}
+	let newArr = [];
+	let i = 0;
+	let y = 0;
+	while(i < arr1.length && y < arr2.length){
+        //compare 2 values and push the smaller value into new arr
+        if(arr1[i] == arr2[y]){
+            newArr.push(arr1[i])
+			i++
+			y++
+        } else if (arr1[i] < arr2[y]){
+			i++
+		} else {
+			y++
 		}
-	}
+    }
 	return newArr;
 }
 
 array1 = [1, 2, 2, 2, 7]
 array2 = [2, 2, 6, 6, 7]
 console.log(intersectSortArr(array1, array2))
+
+function intersectSortArrDedupe(arr1, arr2) {
+	let newArr = [];
+	let i = 0;
+	let y = 0;
+	while(i < arr1.length && y < arr2.length){
+        //compare 2 values and push the smaller value into new arr
+        if(arr1[i] == arr2[y]){
+            newArr.push(arr1[i])
+            i++
+			y++
+        } else {
+			i++
+			y++
+		}
+    }
+	return newArr;
+}
+console.log(intersectSortArrDedupe(array1, array2))
