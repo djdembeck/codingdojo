304493e1156dd595ab6501cd4a0c7c7747c74a76
djdembeck <dembeck.david@protonmail.com>
2021-05-25 10:02:29 -0500

    Hexadecimal System

---
+++ 3-mean/morning-algos/1002.js
@@ -0,0 +1,52 @@
+// Decimal to Hexadecimal
+// 13
+console.log(0xD)
+
+// 6
+console.log(0x6)
+
+// 25
+console.log(0x19)
+
+// 8
+console.log(0x8)
+
+// 45
+console.log(0x2D)
+
+// 10
+console.log(0xA)
+
+// 64
+console.log(0x40)
+
+// 255
+console.log(0xFF)
+
+// 238
+console.log(0xEE)
+
+// Hexadecimal to Decimal
+// 0xDB
+// 219
+
+// 0x5
+// 5
+
+// 0x20C
+// 524
+
+// 0x4F
+// 79
+
+// 0xB2
+// 178
+
+// 0x0
+// 0
+
+// 0x7E
+// 126
+
+// 0x101
+// 257
