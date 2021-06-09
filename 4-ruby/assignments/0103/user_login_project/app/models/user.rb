8f75063788bffd4b7ed3801daefab01569222e52
djdembeck <dembeck.david@protonmail.com>
2021-06-09 11:38:44 -0500

    finish users assignment

---
+++ 4-ruby/assignments/0103/user_login_project/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+	validates :first_name, :last_name, presence: true, length: { minimum: 2 }
+	validates :age, presence: true, numericality: { only_integer: true, greater_than: 10, less_than: 150 }
+	validates :email_address, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+end
