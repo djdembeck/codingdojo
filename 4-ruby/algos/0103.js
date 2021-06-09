ec5b4822c14876c703b44d39cd9b805af36cf19e
djdembeck <dembeck.david@protonmail.com>
2021-06-09 15:26:47 -0500

    finish dojo_ninjas

--- 4-ruby/algos/0103.js
+++ 4-ruby/algos/0103.js
@@ -1,18 +1,37 @@
 function removeDupes(arr) {
-	var newArr = [arr[0]];
-	for (let i = 0; i < arr.length; i++) {
+	var newObj = {};
+	for (let i = arr.length; i > 0; i--) {
 		let count = 0;
-		while (count < newArr.length) {
-			if (newArr[count] == arr[i]) {
+		while (count < Object.keys(newObj).length) {
+			if (newObj[arr[i]]) {
+				console.log(`Plus 1 of ${newObj[arr[i]]}`)
+				newObj[arr[i]] += 1
 				break
 			}
 			count++
-			if (count == newArr.length) {
-				newArr.push(arr[i])
+			if (count == Object.keys(newObj).length) {
+				newObj[arr[i]] = 1
 			}
 		}
 	}
-	return newArr;
+	let count = arr.length - 1
+	console.log(newObj)
+	while (Object.keys(newObj).length > 0) {
+		console.log(`Here's a key ${newObj[arr[count]]}`)
+		if (newObj[arr[count]]) {
+			console.log("Key exist")
+			if (newObj[arr[count]].value > 1) {
+				arr.pop()
+				newObj[arr[count]] -= 1
+			} else {
+				delete newObj[arr[count]]
+			}
+			count--
+		} else {
+			break
+		}
+	}
+	return arr;
 }
 
 array = [1, 2, 1, 3, 4, 2]
