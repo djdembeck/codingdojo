3d0576cb865576b133945c8beb08ad7b42130f3b
djdembeck <dembeck.david@protonmail.com>
2021-06-13 22:23:24 -0500

    Dojo Dashboard II

--- 4-ruby/assignments/0106/dojo_dashboard/app/controllers/dojo_controller.rb
+++ 4-ruby/assignments/0106/dojo_dashboard/app/controllers/dojo_controller.rb
@@ -1,12 +1,21 @@
 class DojoController < ApplicationController
   def index
 	@dojos = Dojo.all
+	@dojo_count = Dojo.all.count
   end
 
   def new
+	@dojo = Dojo.new
   end
 
   def create
+	@dojo = Dojo.new(dojo_params)
+	if @dojo.save
+		flash[:success] = "Dojo submitted successfully"
+		redirect_to '/dojo'
+	else
+		render :new
+	end
   end
 
   def show
@@ -21,3 +30,8 @@ class DojoController < ApplicationController
   def destroy
   end
 end
+
+private
+	def dojo_params
+		params.require(:dojo).permit(:branch, :street, :city, :state)
+	end
--- 4-ruby/assignments/0106/dojo_dashboard/app/models/dojo.rb
+++ 4-ruby/assignments/0106/dojo_dashboard/app/models/dojo.rb
@@ -1,2 +1,4 @@
 class Dojo < ActiveRecord::Base
+	validates :branch, :street, :city, presence: true, length: { minimum: 4 }
+	validates :state, presence: true, length: { is: 2 }
 end
