0309d2d69e6a452efd7013b19c6bfeb9f245aa9f
djdembeck <dembeck.david@protonmail.com>
2021-06-21 00:10:05 -0500

    Assignment: Authentication

--- 4-ruby/assignments/0205/users/app/controllers/users_controller.rb
+++ 4-ruby/assignments/0205/users/app/controllers/users_controller.rb
@@ -6,9 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-  end
+	@user = User.find_by_email(user_params[:email]).try(:authenticate, user_params[:password])
+	if @user
+		session[:user_id] = @user.id
+		flash[:success] = "User added successfully"
+		redirect_to controller: 'users', action: 'show', id: @user.id
+	else
+		flash[:errors] = ['Invalid Combination']
+		redirect_to new_user_path
+	end
+end
 
   def show
+	@user = session[:user]
   end
 
   def edit
@@ -18,5 +28,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
-  end
+	session[:user] = nil
+	redirect_to new_user_path
 end
+end
+private
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
