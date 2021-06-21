0076949f8842a9968e60157d04f94ebcf18dc97c
djdembeck <dembeck.david@protonmail.com>
2021-06-21 16:02:36 -0500

    Assignment: User Features

--- 4-ruby/assignments/0205/users/app/controllers/users_controller.rb
+++ 4-ruby/assignments/0205/users/app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
   end
 
   def create
+	@user = User.new(user_params)
+	if @user.save
+		session[:user_id] = @user.id
+		flash[:success] = "User added successfully"
+		redirect_to new_session_path
+	else
+		flash[:errors] = ["can't be blank"]
+		redirect_to new_user_path
+	end
   end
 
   def show
@@ -21,6 +30,10 @@ class UsersController < ApplicationController
 
   def destroy
 	session[:user] = nil
-	redirect_to new_session_path
+	redirect_to new_user_path
 	end
 end
+private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
