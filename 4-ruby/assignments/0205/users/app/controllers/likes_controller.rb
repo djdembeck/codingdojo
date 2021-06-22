6f9902f4a2158a5840363fd71acd987283697a58
djdembeck <dembeck.david@protonmail.com>
2021-06-22 17:50:18 -0500

    Assignment: Like Authorization (Advanced)

--- 4-ruby/assignments/0205/users/app/controllers/likes_controller.rb
+++ 4-ruby/assignments/0205/users/app/controllers/likes_controller.rb
@@ -12,7 +12,13 @@ class LikesController < ApplicationController
 	end
 
   def destroy
-	Like.where(secret_id: params[:id], user_id: current_user.id).destroy_all
-	redirect_to :back
+	like = Like.where(secret_id: params[:id], user_id: current_user.id)
+	unless like.count == 1
+		flash[:errors] = ["Cannot delete a like you don't own"]
+		redirect_to secrets_path
+	else
+		like.destroy_all
+		redirect_to secrets_path
+	end
   end
 end
--- 4-ruby/assignments/0205/users/spec/controllers/likes_controller_spec.rb
+++ 4-ruby/assignments/0205/users/spec/controllers/likes_controller_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe LikesController, type: :controller do
 		expect(response).to redirect_to("/sessions/new")
 	end
   end
+  context "when signed in as the wrong user" do
+	before do
+		@user2 = create(:user, name: "Test", email: "test@example.com", password: "password", password_confirmation: "password")
+		session[:user_id] = @user2.id
+	end
+    it "shouldn't be able to destroy a like" do
+		delete :destroy, id: @secret
+		expect(response).to redirect_to(secrets_path)
+	end
+  end 
 end
