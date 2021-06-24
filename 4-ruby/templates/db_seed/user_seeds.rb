1800504957d8c8e07d166aee20f8c7d1e23a9c8a
djdembeck <dembeck.david@protonmail.com>
2021-06-24 16:14:08 -0500

    Add password, city and state fields to seeder

--- 4-ruby/templates/db_seed/user_seeds.rb
+++ 4-ruby/templates/db_seed/user_seeds.rb
@@ -1,12 +1,12 @@
 people = [
-	["Danny", "Developer", "danny@example.com"],
-	["Erin", "Engineer", "erin@example.com"],
-	["Megan", "Manager", "megan@example.com"],
-	["Otis", "Operations", "otis@example.com"],
-	["Sally", "Supervisor", "sally@example.com"]
+	["Danny", "Developer", "danny@example.com", "Dallas", "TX"],
+	["Erin", "Engineer", "erin@example.com", "Seattle", "WA"],
+	["Megan", "Manager", "megan@example.com", "Los Angeles", "CA"],
+	["Otis", "Operations", "otis@example.com", "Atlanta", "GA"],
+	["Sally", "Supervisor", "sally@example.com", "Miami", "FL"]
 ]
 
 people.each do |person|
 	puts "Inserting #{person[0]} #{person[1]} into the DB"
-	User.create(first_name: person[0], last_name: person[1])
+	User.create(first_name: person[0], last_name: person[1], email: person[2], password: person[2], city: person[3], state: person[4])
 end
