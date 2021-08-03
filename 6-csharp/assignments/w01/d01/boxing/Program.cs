82f570d75bcac3380281e8cad55c5b19872a3c72
djdembeck <dembeck.david@protonmail.com>
2021-08-03 01:18:18 -0500

    Start of boxing

---
+++ 6-csharp/assignments/w01/d01/boxing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Empty list
+            List<string> list = new List<string>();
+            // List of type object
+            object ActuallyList = list;
+        }
+    }
+}
