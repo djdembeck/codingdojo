eb4cbec0008f0befbcd49e3620e65a86e2c1835b
djdembeck <dembeck.david@protonmail.com>
2021-08-04 22:45:28 -0500

    finish wizard ninja samurai

--- 6-csharp/assignments/w01/d02/WizardNinjaSamurai/Program.cs
+++ 6-csharp/assignments/w01/d02/WizardNinjaSamurai/Program.cs
@@ -14,11 +14,19 @@ namespace WizardNinjaSamurai
         {
             Console.WriteLine("Attack");
             int damage = 5 * Intelligence;
-            TakeDamage(damage);
+            target.TakeDamage(damage);
             // Heal self
             health += damage;
             Console.WriteLine($"Health of target is now {target.Health}");
             return target.Health;
+        }
+
+        public void Heal(Human target)
+        {
+            Console.WriteLine("Healing...");
+            // Nifty trick to instead add to health :)
+            int amountToHeal = Intelligence * 10;
+            target.TakeDamage(amountToHeal * -1);
         }
     }
 
@@ -28,12 +36,53 @@ namespace WizardNinjaSamurai
         {
             Dexterity = 175;
         }
+
+        public override int Attack(Human target)
+        {
+            Random rand = new Random();
+            int randidx = rand.Next(1,11);
+            if (randidx < 2)
+            {
+                Console.WriteLine("Dealing extra damage!");
+                target.TakeDamage(10);
+            }
+            Console.WriteLine("Attack");
+            int damage = 5 * Dexterity;
+            target.TakeDamage(damage);
+            // Heal self
+            health += damage;
+            Console.WriteLine($"Health of target is now {target.Health}");
+            return target.Health;
+        }
+
+        public void Steal(Human target)
+        {
+            Console.WriteLine("Swiper no swiping!");
+            target.TakeDamage(5);
+            health += 5;
+        }
     }
 
     class Samurai : Human
     {
         public Samurai(string name) : base(name)
         {
+            health = 200;
+        }
+
+        public override int Attack(Human target)
+        {
+            base.Attack(target);
+            if (target.Health < 50)
+            {
+                target.TakeDamage(target.Health);
+            }
+            return target.Health;
+        }
+
+        public void Meditate()
+        {
+            Console.WriteLine("Hummmmmmmmm");
             health = 200;
         }
     }
