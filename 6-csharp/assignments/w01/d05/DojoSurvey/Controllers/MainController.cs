e20be160137ae3091d8d828dd4b7c69c49a05b81
djdembeck <dembeck.david@protonmail.com>
2021-08-08 14:53:25 -0500

    DojoSurvey with validations

--- 6-csharp/assignments/w01/d05/DojoSurvey/Controllers/MainController.cs
+++ 6-csharp/assignments/w01/d05/DojoSurvey/Controllers/MainController.cs
@@ -17,7 +17,20 @@ namespace DojoSurvey.Controllers
             return View();
         }
 
-        [HttpPost("show")]
+        [HttpPost("survey/create")]
+        public IActionResult Create(Survey survey)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Show", survey);
+            }
+            else
+            {
+                return View("New");
+            }
+        }
+
+        [HttpGet("show")]
         public IActionResult Show(Survey survey)
         {
             return View(survey);
--- 6-csharp/assignments/w01/d05/DojoSurvey/Models/Survey.cs
+++ 6-csharp/assignments/w01/d05/DojoSurvey/Models/Survey.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DojoSurvey.Models
 {
     public class Survey
     {
+        [Required]
+        [MinLength(2)]
         public string Name {get;set;}
+        [Required]
         public string Location {get;set;}
+        [Required]
+        [Display(Name = "Favorite Languages:")]
         public string FavLang {get;set;}
+        [MinLength(20)]
         public string Comment {get;set;}
     }
 }
