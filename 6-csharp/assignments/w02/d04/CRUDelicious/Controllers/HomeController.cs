214c3934ac4a915f8d4c5697bd73919c2f7a105a
djdembeck <dembeck.david@protonmail.com>
2021-08-13 17:07:51 -0500

    Finish CRUDelicious

--- 6-csharp/assignments/w02/d04/CRUDelicious/Controllers/HomeController.cs
+++ 6-csharp/assignments/w02/d04/CRUDelicious/Controllers/HomeController.cs
@@ -24,8 +24,84 @@ namespace CRUDelicious.Controllers
         public IActionResult Index()
         {
             List<Dish> AllDishes = _context.Dishes.ToList();
-            
+
+            ViewBag.RecentDishes = _context.Dishes
+            .OrderByDescending(d => d.CreatedAt)
+            .Take(5)
+            .ToList();
             return View();
+        }
+
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Dish dish)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(dish);
+                _context.SaveChanges();
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View("New");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Show(int id)
+        {
+            ViewBag.ThisDish = _context.Dishes
+            .SingleOrDefault(d => d.DishId == id);
+            return View();
+        }
+
+        [HttpGet("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            Dish ThisDish = _context.Dishes
+            .SingleOrDefault(d => d.DishId == id);
+
+            _context.Dishes.Remove(ThisDish);
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet("edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            Dish ThisDish = _context.Dishes
+            .SingleOrDefault(d => d.DishId == id);
+            return View(ThisDish);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Update(int id, Dish dish)
+        {
+            Dish ThisDish = _context.Dishes
+            .SingleOrDefault(d => d.DishId == id);
+
+            if(ModelState.IsValid)
+            {
+                ThisDish.Chef = dish.Chef;
+                ThisDish.Name = dish.Name;
+                ThisDish.Tastiness = dish.Tastiness;
+                ThisDish.Calories = dish.Calories;
+                ThisDish.Description = dish.Description;
+                _context.SaveChanges();
+                return RedirectToAction("show", new { id = id });
+            }
+            else
+            {
+                return View();
+            }
+
         }
     }
 }
--- 6-csharp/assignments/w02/d04/CRUDelicious/Models/Dish.cs
+++ 6-csharp/assignments/w02/d04/CRUDelicious/Models/Dish.cs
@@ -6,10 +6,17 @@ namespace CRUDelicious.Models
     {
         [Key]
         public int DishId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Chef { get; set; }
+        [Required]
+        [Range(1, 5)]
         public int Tastiness { get; set; }
+        [Required]
+        [Range(1, Int32.MaxValue)]
         public int Calories { get; set; }
+        [Required]
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
