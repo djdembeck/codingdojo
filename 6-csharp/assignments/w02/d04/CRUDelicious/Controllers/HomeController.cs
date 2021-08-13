406043ea64ad849f5fb407086799bf812a22f2e6
djdembeck <dembeck.david@protonmail.com>
2021-08-12 23:26:11 -0500

    Start of CRUDelicious

---
+++ 6-csharp/assignments/w02/d04/CRUDelicious/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CRUDelicious.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDelicious.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Context _context;
+
+        // here we can "inject" our context service into the constructor
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            List<Dish> AllDishes = _context.Dishes.ToList();
+            
+            return View();
+        }
+    }
+}
